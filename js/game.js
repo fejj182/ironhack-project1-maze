497d55885a3bb73edaad4505772538af045d2361
Jeff <jzinger100@gmail.com>
2017-01-29 20:54:29 +0100

    game generates new maze on completion

--- js/game.js
+++ js/game.js
@@ -2,24 +2,30 @@ $(document).ready(function(){
 
 	// Draw maze
 
-	var myMaze = createMaze();
-	var disp = myMaze.visitedCells;
-	for (var i = 0; i < disp.length; i++) {
-			$('#maze > tbody').append("<tr>");
-			for (var j = 0; j < disp[i].length; j++) {
-					var selector = i+"-"+j;
-					$('#maze > tbody').append("<td id='"+selector+"'>&nbsp;</td>");
-					if (disp[i][j][0] == 0) { $('#'+selector).css('border-top', '2px solid black'); }
-					if (disp[i][j][1] == 0) { $('#'+selector).css('border-right', '2px solid black'); }
-					if (disp[i][j][2] == 0) { $('#'+selector).css('border-bottom', '2px solid black'); }
-					if (disp[i][j][3] == 0) { $('#'+selector).css('border-left', '2px solid black'); }
-			}
-			$('#maze > tbody').append("</tr>");
+	var myMaze = createMaze(), disp, selector;
+
+	function drawMaze(myMaze) {
+		disp = myMaze.visitedCells;
+		$("#maze > tbody").remove();
+		$("#maze").append("<tbody></tbody>");
+		for (var i = 0; i < disp.length; i++) {
+				$("#maze > tbody").append("<tr>");
+				for (var j = 0; j < disp[i].length; j++) {
+						selector = i+"-"+j;
+						$("#maze > tbody").append("<td id='"+selector+"'>&nbsp;</td>");
+						if (disp[i][j][0] == 0) { $("#"+selector).css("border-top", "2px solid black"); }
+						if (disp[i][j][1] == 0) { $("#"+selector).css("border-right", "2px solid black"); }
+						if (disp[i][j][2] == 0) { $("#"+selector).css("border-bottom", "2px solid black"); }
+						if (disp[i][j][3] == 0) { $("#"+selector).css("border-left", "2px solid black"); }
+				}
+				$("#maze > tbody").append("</tr>");
+		}
+		$("#0-0").css("border-top","none");
+		$("#" + selector).css("border-right","none");
+		$("#" + selector).html("<i class='fa fa-child' aria-hidden='true'></i>");
 	}
 
-	$("#0-0").css("border-top","none");
-	$("#19-19").css("border-right","none");
-	$("#19-19").html("<i class='fa fa-child' aria-hidden='true'></i>");
+	drawMaze(myMaze);
 
 	///////////////////
 	// Items and weapons
@@ -57,25 +63,30 @@ $(document).ready(function(){
 	}
 
 	Player1.prototype.checkForWall = function(direction){
-		var checkSelector = this.location[0] + "-" + (player.location[1]);
+		var checkSelector = this.location[0] + "-" + (this.location[1]);
 		var border = $("#" + checkSelector).css("border-" + direction);
 		var borderCheck = border.indexOf("2px") == -1;
-
 		return borderCheck;
 	}
 
 	Player1.prototype.movePlayer = function(){
 
 	// move function used in multiple locations within bind function below
-	function move(newLocationCommand) {
-		$("#" + selector).html("");
-		newLocationCommand;
-		//overwrite selector after move
-		selector = player.location[0] + '-' + player.location[1];
-		$("#" + selector).html("<i class='fa fa-child' aria-hidden='true'></i>");
-	}
+		function move(newLocationCommand) {
+			$("#" + selector).html("");
+			newLocationCommand;
+			//overwrite selector after move
+			selector = player.location[0] + "-" + player.location[1];
+			$("#" + selector).html("<i class='fa fa-child' aria-hidden='true'></i>");
+		}
 
-		$(document).bind('keyup', function(e){
+		function newMaze(){
+			player.location = [(disp[0].length-1),(disp.length-1)];
+			var newMaze = createMaze();
+			drawMaze(newMaze);
+		}
+
+		$(document).bind("keyup", function(e){
 			var selector = player.location[0] + "-" + player.location[1];
 				if(e.which==37 && player.checkForWall("left")) {
 					//left
@@ -84,8 +95,11 @@ $(document).ready(function(){
 				if(e.which==38 && player.checkForWall("top")) {
 					//up
 					move(player.location[0]--);
+					if (player.location[0] < 0) {
+						newMaze();
+					}
 				}
-				if(e.which==39 && player.checkForWall("right")) {
+				if(e.which==39 && player.checkForWall("right") && (player.location[1]+1) < myMaze.columns) {
 					//right
 					move(player.location[1]++);
 				}
@@ -96,23 +110,23 @@ $(document).ready(function(){
 		})
 	}
 
-	var player = new Player1("Jeff",50,weapons[0]);
-	console.log(player);
-	player.movePlayer();
+		var player = new Player1("Jeff",50,weapons[0]);
+		console.log(player);
+		player.movePlayer();
 
-	/////////////////
-	//Enemy Prototype
-	/////////////////
+		/////////////////
+		//Enemy Prototype
+		/////////////////
 
-	function Enemy(name,health,weapon) {
-		Character.call(this,name,health,weapon);
-		this.name = name;
-		this.health = health;
-		this.weapon = weapon;
-	}
+		function Enemy(name,health,weapon) {
+			Character.call(this,name,health,weapon);
+			this.name = name;
+			this.health = health;
+			this.weapon = weapon;
+		}
 
-	var goblin = new Enemy("Goblin",20,weapons[1]);
-	console.log(goblin);
+		var goblin = new Enemy("Goblin",20,weapons[1]);
+		console.log(goblin);
 
 
 })
